7a20eb45aa6b478f394daaff0236d6c57b99c35f
galaxyunicorn42 <marisol.vargas@amethodschools.org>
2020-01-30 02:25:34 +0000

    the playlist

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
-let songName=["marigold","sleepwalk","veryape","memories"];
-let artistName=["nirvana","santo&jonny","nirvana","panicatthedisco"];
-songName.forEach(function(songName){
-$(".songsName").append("<p>" +songName+ "</p>");
-});
-artistName.forEach(function(artistName){
-$(".artistName").append("<p>" + artistName + "</p>");
-});
+// let songName=["marigold","stray heart","very ape","memories"];
+// let artistName=["nirvana","green day","nirvana","panicat the disco"];
+// songName.forEach(function(songName){
+// $(".songsName").append("<p>" +songName+ "</p>");
+// });
+// artistName.forEach(function(artistName){
+// $(".artistName").append("<p>" + artistName + "</p>");
+// });
+//  $(".theButton").click(function(){
+//  });
+ let songName ={
+     artist: "nirvana",
+     songName: "marigold",
+     img: "https://cdn.mos.cms.futurecdn.net/ycVyeFhpf9Q5XcTjdqSr75-320-80.jpg",
+     link:"https://www.youtube.com/watch?v=jEsOww93NM0",
+     songlength: 10
+     
+ }
+ let songName ={
+     artist: "green day",
+     songName: "stray heart",
+     img: "https://www.nme.com/wp-content/uploads/2019/09/green_day_2000.jpg",
+     link: "https://www.youtube.com/watch?v=sN0b-adUt9I",
+     songlength: 10
+     
+ }
+ let songName ={
+     artist: "nirvana",
+     songName: "very ape",
+     img: "https://img.apmcdn.org/9366269874562bbfd3d954e8a7b1a0d8d8f2bce3/uncropped/a21c90-20130927-steve-albini-letter.jpg",
+     link: "https://www.youtube.com/watch?v=91yrS5PUJBY",
+     songlength:10 
+ }
+let songName ={
+    artist: "panic at the disco",
+    songName: "hurricane",
+    img: "https://dobienews.scuc.txed.net/wp-content/uploads/2017/12/panic2-720x900.jpg",
+    link: "https://www.youtube.com/results?search_query=panic+at+the+disco+hurricane",
+    songlength: 10
+}
